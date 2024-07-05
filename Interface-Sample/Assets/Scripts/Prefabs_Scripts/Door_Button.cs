1cdb2ae7548aabdec01144394eda3852836a58c7
AKDasan <akdasan@icloud.com>
2024-07-05 18:46:56 +0300

    The code now interacts with the player.

--- Interface-Sample/Assets/Scripts/Prefabs_Scripts/Door_Button.cs
+++ Interface-Sample/Assets/Scripts/Prefabs_Scripts/Door_Button.cs
@@ -1,12 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Assertions.Must;
 
 public class Door_Button : MonoBehaviour, IInteractable
 {
+    private Renderer buttonRenderer;
+
+    [SerializeField] Material button_Deactive;      // Kapý kapalý ise kýrmýzý material
+    [SerializeField] Material button_Active;        // Kapý açýk ise yeþil material
+
+    private void Awake()
+    {
+        buttonRenderer = GetComponent<Renderer>();
+        buttonRenderer.material = button_Deactive;
+    }
+
     public void Interact()
     {
         // Door_button'a deðildiðinde yapýlacak iþlemler.
-             
+        buttonRenderer.material = button_Active;
+        
+        Transform parent = transform.parent;
+
+        if (parent != null )
+        {
+            transform.parent = null;
+            Destroy(parent.gameObject);
+        }
     }
 }
